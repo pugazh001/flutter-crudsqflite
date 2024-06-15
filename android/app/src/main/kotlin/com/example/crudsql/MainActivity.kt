ca978f320bc32ef99c018eeea8917fc4a4cd274a
pugazh001 <ajinkyapugal27@gmail.com>
2024-06-15 16:40:34 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/crudsql/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.crudsql
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/services/userService.dart
@@ -0,0 +1,30 @@
+
+//import 'dart:async';
+
+import 'package:crudsql/db_helper/repository.dart';
+import 'package:crudsql/model/User.dart';
+
+class UserService
+{
+  late Repository _repository;
+  UserService(){
+    _repository = Repository();
+  }
+  //Save User
+  SaveUser(User user) async{
+    return await _repository.insertData('users', user.userMap());
+  }
+  //Read All Users
+  readAllUsers() async{
+    return await _repository.readData('users');
+  }
+  //Edit User
+  UpdateUser(User user) async{
+    return await _repository.updateData('users', user.userMap());
+  }
+
+  deleteUser(userId) async {
+    return await _repository.deleteDataById('users', userId);
+  }
+
+}
